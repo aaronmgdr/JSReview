36cea5a0c12318a920f84b2a589fa5442657790c
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-18 15:05:12 -0500

    introduces 2 minor errors

--- review.js
+++ review.js
@@ -5,15 +5,28 @@
 // console.log("1 plus 2 is:", add(1, 2));
 // console.log("11 plus 4 is:", add(11, 4));
 
-var person = {
-  name: "Bob",
-  jumpCount: 0,
+// var person = {
+//   name: "Bob",
+//   jumpCount: 0,
   
-  jump: function () {
-    this.jumpCount += 1;
-  }
+//   jump: function () {
+//     this.jumpCount += 1;
+//   }
+// };
+
+// person.jump();
+// person.jump();
+// console.log(person.name + " should have jumped twice:" + person.jumpCount);
+
+
+var game = {};
+game.wins = 0;
+game.incremntWins = function (amount) {
+  wins += amount;
 };
 
-person.jump();
-person.jump();
-console.log(person.name + " should have jumped twice:" + person.jumpCount);
+game.incrementWins(2);
+console.log("Game wins should be 2:", person.jumpCount);
+
+game.incrementWins(7);
+console.log("Game wins should be 9:", person.jumpCount);
