5c9960d478da3503fd3b2762c717cd3ee733c683
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-18 19:27:09 -0500

    returns surprise

--- review.js
+++ review.js
@@ -115,24 +115,103 @@
 // Deducts energy by that amount, unless amount is greater than energy
 // Javascript has no unless keyword.
 
-var createBattery = function (charge) {
-	return {
-		energy: charge,
-		use: function (amount) {
-			this.energy >= amount ? this.energy -= amount : "C'est Impossible"
-		}
-	}
+// var createBattery = function (charge) {
+// 	return {
+// 		energy: charge,
+// 		use: function (amount) {
+// 			this.energy >= amount ? this.energy -= amount : "C'est Impossible"
+// 		}
+// 	}
+// };
+
+// var battery = createBattery(3);
+// console.log('Battery should have 3 energy:', battery.energy);
+
+// battery.use(2);
+// console.log('Battery should have 1 energy:', battery.energy);
+
+// battery.use(5);
+// console.log('Battery should have 1 energy:', battery.energy);
+
+// Individual Exercise #9
+// Create a Dog constructor that:
+
+// Takes one parameter name
+// Has a property name that points to a string
+// Has a property hunger that points to the number 10
+// Has a property feed that points to a function:
+// Takes one parameter food
+// If food is 'steak', deduct hunger by two
+// If food is 'toy', deduct hunger by zero
+// Else deduct hunger by one
+// Do not allow hunger to go below zero
+// If hunger is zero, alert a snooze string (ex: zZzzZz)
+
+// var Dog = function (name) {
+// 		return {
+// 			name: name,
+// 			hunger: 10,
+// 			feed: function (food) {
+// 				if (this.hunger == 0) {
+// 					alert("zZzzZz");
+// 				} //ends
+
+// 				else 	
+				
+// 				{	
+// 					if (food == 'steak' && this.hunger >= 2) {
+// 						this.hunger -= 2
+// 					} // ends if 
+// 					else if (food == 'toy'){
+// 						this.hunger == this.hunger
+// 					} // ends else if
+// 					else if {
+// 						this.hunger -= 1 
+// 					} 
+// 					else if (this.hunger == 0) {
+// 						alert("zZzzZz");
+// 					}
+// 					 // ends else statement
+// 				} // ends else statement
+// 			} //ends feed : function
+// 		}
+// }; // ends dog function
+
+
+// var billy = new Dog('Billy');
+// console.log("Billy should have 10 hunger points:", billy.hunger);
+
+// billy.feed('steak');
+// console.log("Billy should have 8 hunger points:", billy.hunger);
+
+// billy.feed('toy');
+// console.log("Billy should have 8 hunger points:", billy.hunger);
+
+// billy.feed('bug');
+// console.log("Billy should have 7 hunger points:", billy.hunger);
+
+// billy.feed('steak');
+// billy.feed('steak');
+// billy.feed('steak');
+// // You should see an alert after this next line
+// billy.feed('steak');
+// console.log("Billy should have 0 hunger points:", billy.hunger);
+
+
+// Individual Exercise #10
+// Fix the following code (3 minor errors) and complete result so that the code outputs "Surprise!"
+var house = {
+  room: {
+    emptyBox: {},
+    size: '50 sqft'
+  },
+  otherRoom: {
+    box: {
+      writtenNote: "Surprise!"
+    },
+    size: '35 sqft'
+  }
 };
 
-var battery = createBattery(3);
-console.log('Battery should have 3 energy:', battery.energy);
-
-battery.use(2);
-console.log('Battery should have 1 energy:', battery.energy);
-
-battery.use(5);
-console.log('Battery should have 1 energy:', battery.energy);
-
-
-
-
+var result = house.otherRoom.box.writtenNote;
+console.log("You find a written note:", result);
